051b8cdf9caaf2ba688e285715bfb7d5767dd9e1
Bryan Apperson <frostspyre@gmail.com>
2016-03-21 23:47:06 -0400

    application structure extended

--- site/item_catalog/models.py
+++ site/item_catalog/models.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-
 """Data models for the item_catalog application."""
 
+from flask_sqlalchemy import SQLAlchemy
 from item_catalog import app
+
+# Global database
+DB = SQLAlchemy(app)
