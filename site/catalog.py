05b65bd9bb5b4a9a030c87102a17f6be36fbc01e
Bryan Apperson <bryan@bryanapperson.com>
2016-03-21 22:36:26 -0400

    initial files

---
+++ site/catalog.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""A catalog website module.
+
+This catalog website module provides a list of items within a
+variety of categories as well as provide a user registration and
+authentication system. Registered users have the ability to post,
+edit and delete their own items.
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+# @app.route('/')
+@app.route('/hello')
+def HelloWorld():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
