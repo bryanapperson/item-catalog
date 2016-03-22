3281bb9de5c815e3bb227991e0196e70349fa4f9
Bryan Apperson <bryan@bryanapperson.com>
2016-03-22 01:44:51 -0400

    add static assets and begin templates, data models

--- catalog/item_catalog/models.py
+++ catalog/item_catalog/models.py
@@ -8,6 +8,30 @@ from item_catalog import app
 DB = SQLAlchemy(app)
 
 
+class CatalogMeta(DB.Model):
+    """Class to represent web application metadata.
+
+    Description: Can be used to add properties with a value. Such as
+                 the web application's title.
+
+    Inputs: <propertyName> - a unique string for the name of a property to
+                             create.
+            <value> - a string for a value associated with the propertyName
+    """
+    id = DB.Column(DB.Integer, primary_key=True)
+    propertyName = DB.Column(DB.String(80), unique=True)
+    value = DB.Column(DB.String(120))
+
+    def __init__(self, propertyName, value):
+        """Constructor function for User."""
+        self.propertyName = propertyName
+        self.value = value
+
+    def __repr__(self):
+        """Information about this class."""
+        return '<Property %r>' % self.propertyName
+
+
 class User(DB.Model):
     """Class to represent web application user."""
     id = DB.Column(DB.Integer, primary_key=True)
--- catalog/item_catalog/views.py
+++ catalog/item_catalog/views.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 """Views for the item_catalog application."""
 
+from flask import render_template
 from item_catalog import app
 
 
 @app.route('/')
 def index():
     """Index page view."""
-    return 'Hello World!'
+    return render_template('index.html')
