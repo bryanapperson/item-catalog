e06fd989b077a248955472b1f80f0730bd794242
Bryan Apperson <frostspyre@gmail.com>
2016-03-23 01:34:00 -0400

    adding sample data

--- catalog/item_catalog/db_actions.py
+++ catalog/item_catalog/db_actions.py
@@ -20,34 +20,59 @@ def recent_items(number=10):
         number)
     return recent
 
+# Begin sample data section
 
-def sample_data():
-    """Sample data for initial database population."""
+
+def sample_metadata():
+    """Populate metadata table with sample rows."""
+    pass
+
+
+def sample_categories():
+    """Sample category data."""
+    # List of category names
     category_list = ['Soccer', 'Basketball', 'Baseball', 'Frisbee',
                      'Snowboarding', 'Rock Climbing', 'Foosball', 'Skating',
                      'Hockey']
-    price_list = ['4.99', '5.99', '49.99', '59.99', '4.99', '5.99', '49.99',
-                  '59.99', '4.99', '5.99', '49.99', '59.99']
-    item_category_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-    item_name_list = ['thing1', 'thing1', 'thing1', 'thing1', 'thing1',
-                      'thing1', 'thing1', 'thing1', 'thing1', 'thing1',
-                      'thing1', 'thing1']
+    for category in category_list:
+        categoryobj = models.Category(name=category)
+        models.DB.session.add(categoryobj)
+        models.DB.session.commit()
+
+
+def sample_items():
+    """Sample item data."""
+    # Information for items
+    price_list = ['18.99', '21.99', '6.99', '12.99', '499.99', '69.99',
+                  '999.99', '199.99', '69.99', '129.99', '139.99', '9.99']
+    item_category_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 2, 4, 8]
+    item_name_list = ['Soccer Ball', 'Basketball', 'Baseball', 'Frisbee',
+                      'Snowboard', 'Grappling Hook', 'Foosball Table',
+                      'Ice Skates', 'Hockey Stick', 'Catcher\'s Mit',
+                      'Clip in Boots', 'Hockey Puck']
     desc_list = ['desc', 'desc', 'desc', 'desc', 'desc', 'desc', 'desc',
                  'desc', 'desc', 'desc', 'desc', 'desc']
-    for price, category, name, desc in zip(price_list,
-                                           item_category_list,
-                                           item_name_list,
-                                           desc_list):
+    # Let's be pythonic about populating items
+    for price, category, name, desc in zip(price_list, item_category_list,
+                                           item_name_list, desc_list):
         itemobj = models.CatalogItem(name=name,
                                      category_id=category,
                                      price=price,
                                      description=desc)
         models.DB.session.add(itemobj)
         models.DB.session.commit()
-    for category in category_list:
-        categoryobj = models.Category(name=category)
-        models.DB.session.add(categoryobj)
-        models.DB.session.commit()
+
+
+def sample_data():
+    """Sample data for initial database population."""
+    # Populate metadata
+    sample_metadata()
+    # Populate categories
+    sample_categories()
+    # Populate items
+    sample_items()
+
+# End sample data section
 
 
 def create_db():
