77d974dac4db3901c802c13e62fa57fd6dca6cd3
Bryan Apperson <frostspyre@gmail.com>
2016-03-23 00:34:03 -0400

    changes to data models

--- catalog/item_catalog/db_actions.py
+++ catalog/item_catalog/db_actions.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 """Database actions module."""
 from item_catalog import models
+from item_catalog import app
+import os
 
 
 def all_category_infomation():
@@ -16,3 +18,23 @@ def recent_items(number=10):
     recent = models.DB.session.query(items).order_by(items.id.
                                                      desc()).limit(number)
     return recent
+
+
+def sample_data():
+    """Sample data for initial database population."""
+    pass
+
+
+def model_population():
+    """Populate the DB if it is SQLite and does not exist."""
+    db_string = app.config['SQLALCHEMY_DATABASE_URI']
+    if 'sqlite:' in db_string:
+        db_string = db_string.split('/')[-1]
+        db_string = os.getcwd() + '/item_catalog/' + db_string
+        if not os.path.isfile(db_string):
+            models.create_db()
+            print ("Creating DB.")
+
+
+# Call DB population check
+model_population()
